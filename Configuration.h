525f067bd272e9c7bad0dc26cedb647c375e8016
Patrick Hätti <91842959+patrickhaetti@users.noreply.github.com>
2024-02-14 10:21:07 +0100

    Update Configuration.h
    GRID_MAX_POINTS_X 5

--- Configuration.h
+++ Configuration.h
@@ -127,7 +127,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "20240213_fw_update"
+#define CUSTOM_MACHINE_NAME "20240214_fw_update"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -2139,7 +2139,7 @@
 #if ANY(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
